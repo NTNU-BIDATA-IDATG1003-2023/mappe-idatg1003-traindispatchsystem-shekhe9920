8828becd03be2615962f353b21160701c7ecb0ac
shekhe9920 <karwans@stud.ntnu.no>
2023-10-14 23:39:11 +0200

    Adjust departure time handling to accommodate delays.
    This commit modifies the departure time handling to allow for the possibility of adding delays in case a train is not on time This change is necessary to provide accurate information about train delays and prevent unnecessary complications.

--- src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchSystem.java
@@ -11,107 +11,127 @@ import java.time.LocalTime;
  *
  */
 public class TrainDispatchSystem {
-    private String departureStation;
-    private String destination;
-    private LocalTime departureTime;
-    private String line;
-    private int track;
-    private int trainNumber; // Store the train number directly
-    private boolean trainNumberSet = false; // Boolean flag to track if train number is set
+  private String departureStation;
+  private String destination;
+  private LocalTime departureTime;
+  private String line;
+  private int track;
+  private int trainNumber; // Store the train number directly
+  private boolean trainNumberSet = false; // Boolean flag to track if train number is set
 
-    /**
-     * Constructor to initialize a TrainDispatchSystem object.
-     *
-     * @param departureStation The departure station name.
-     * @param destination      The destination station name.
-     * @param departureTime    The departure time.
-     * @param line             The train line name.
-     * @param track            The track number.
-     * @param trainNumber      The train number.
-     */
+  /**
+  * Constructor to initialize a TrainDispatchSystem object.
+  *
+  * @param departureStation The departure station name.
+  * @param destination      The destination station name.
+  * @param departureTime    The departure time.
+  * @param line             The train line name.
+  * @param track            The track number.
+  * @param trainNumber      The train number.
+  */
 
-    public TrainDispatchSystem(String departureStation, String destination,
+  public TrainDispatchSystem(String departureStation, String destination,
                                LocalTime departureTime, String line, int track, int trainNumber) {
-        this.departureStation = departureStation;
-        this.departureTime = departureTime;
-        this.destination = destination;
-        setLine(line);
-        setTrack(track);
-        setTrainNumber(trainNumber);
-    }
+    this.departureStation = departureStation;
+    setDepartureTime(departureTime);
+    this.destination = destination;
+    setLine(line);
+    setTrack(track);
+    setTrainNumber(trainNumber);
+  }
 
-    // Getter methods
-    public String getDepartureStation() {
-        return departureStation;
-    }
+  // Getter methods
+  public String getDepartureStation() {
+    return departureStation;
+  }
 
-    public String getDestination() {
-        return destination;
-    }
+  public String getDestination() {
+    return destination;
+  }
 
-    public LocalTime getDepartureTime() {
-        return departureTime;
-    }
+  public LocalTime getDepartureTime() {
+    return departureTime;
+  }
 
-    public int getTrainNumber() {
-        return trainNumber;
-    }
+  public int getTrainNumber() {
+    return trainNumber;
+  }
 
-    public String getLine() {
-        return line;
-    }
+  public String getLine() {
+    return line;
+  }
 
-    public int getTrack() {
-        return track;
-    }
-    // Setters methods
-    /**
-     * Sets the train number if it hasn't been set already.
-     *
-     * @param trainNumber                 The train number to set.
-     * @throws IllegalArgumentException   If the train number has already been set.
-     */
-    public void setTrainNumber(int trainNumber) {
-        if (!trainNumberSet) {
-            this.trainNumber = trainNumber;
-            trainNumberSet = true;
-        } else {
-            throw new IllegalStateException("Train number has already been set.");
-        }
-    }
+  public int getTrack() {
+    return track;
+  }
 
-    /**
-     * Sets the track number within the range [1, 10].
-     *
-     * @param track                       The number to set
-     * @throws IllegalArgumentException   If the track number is outside the valid range.
-     */
-    public void setTrack(int track) {
-        if (track >= 1 && track <= 10) {
-            this.track = track;
-        } else {
-            throw new IllegalArgumentException("Track must be between 1 and 10 inclusive.");
-        }
-    }
+  // Setters methods
 
-    /**
-     * Sets the train line name. If line is null, it sets a default value.
-     *
-     * @param line The train line name to set.
-     */
-    public void setLine(String line) {
-        this.line = (line != null) ? line : "Default"; // Sets default value if line is null
-    }
+  /**
+  * Sets the departure time.
+  *
+  * @param departureTime The departure time to set
+  */
+  public void setDepartureTime(LocalTime departureTime) {
+    this.departureTime = departureTime;
+  }
 
-    /**
-     * Generates a string representation of the TrainDispatchSystem object.
-     *
-     * @return A string with details about the train dispatch.
-     */
-    @Override
-    public String toString() {
-        return "Departure from " + departureStation + " to "
-                + destination + " at " + departureTime + "|| Line: "
-                + line + " ||" + "|| Track: " + track + " ||" + "Train number: " + trainNumber;
+  /**
+  * Sets a new departure time if there is any delay.
+  *
+  * @param delayMinutes The delay in minutes to set
+  */
+  public void setDelay(int delayMinutes) {
+    this.departureTime = departureTime.plusMinutes(delayMinutes);
+  }
+  /**
+  * Sets the train number if it hasn't been set already.
+  *
+  * @param trainNumber                 The train number to set.
+  * @throws IllegalArgumentException   If the train number has already been set.
+  */
+
+  public void setTrainNumber(int trainNumber) {
+    if (!trainNumberSet) {
+      this.trainNumber = trainNumber;
+      trainNumberSet = true;
+    } else {
+      throw new IllegalStateException("Train number has already been set.");
     }
+  }
+
+  /**
+  * Sets the track number within the range [1, 10].
+  *
+  * @param track                       The number to set
+  * @throws IllegalArgumentException   If the track number is outside the valid range.
+  */
+  public void setTrack(int track) {
+    if (track >= 1 && track <= 10) {
+      this.track = track;
+    } else {
+      throw new IllegalArgumentException("Track must be between 1 and 10 inclusive.");
+    }
+  }
+
+  /**
+  * Sets the train line name. If line is null, it sets a default value.
+  *
+  * @param line The train line name to set.
+  */
+  public void setLine(String line) {
+    this.line = (line != null) ? line : "Default"; // Sets default value if line is null
+  }
+
+  /**
+  * Generates a string representation of the TrainDispatchSystem object.
+  *
+  * @return A string with details about the train dispatch.
+  */
+  @Override
+  public String toString() {
+    return "Departure from " + departureStation + " to "
+              + destination + " at " + departureTime + "|| Line: "
+              + line + " ||" + "|| Track: " + track + " ||" + "Train number: " + trainNumber;
+  }
 }
--- src/test/java/edu/ntnu/stud/EnhetsTester.java
+++ src/test/java/edu/ntnu/stud/EnhetsTester.java
@@ -16,14 +16,14 @@ public class EnhetsTester {
         TrainDispatchSystem trainDispatch0 = new TrainDispatchSystem("Gjøvik", "Oslo", LocalTime.of(8,
                 55),"F1", 4, 123);
         TrainDispatchSystem trainDispatch1 = new TrainDispatchSystem("Gjøvik", "Bergen", LocalTime.of(8,
-                55),"F1", 2, 123);
+                55),"F1", 2, 13);
 
         // Attempt to set the train number for both objects to 123.
         trainDispatchList.add(trainDispatch0);
         trainDispatchList.add(trainDispatch1);
 
-        trainDispatch0.setTrainNumber(123);
-        trainDispatch1.setTrainNumber(123);
+        //trainDispatch0.setTrainNumber(123);
+        //trainDispatch1.setTrainNumber(13);
 
         // Print the list of TrainDispatchSystem objects.
         System.out.println(trainDispatchList);
@@ -37,6 +37,10 @@ public class EnhetsTester {
             System.out.println(trainDispatchFromList.getLine());
             System.out.println(trainDispatchFromList.getTrainNumber());
         }
+
+        //sets a new departure time if the train is delayed.
+        trainDispatch1.setDelay(10);
+        System.out.println(trainDispatch1.getDepartureTime());
 
 
     }
